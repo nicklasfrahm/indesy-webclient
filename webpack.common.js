49d9a11769aac77e9a647138b4d745c2bf69dc91
nicklasfrahm <nicklas.frahm@gmail.com>
2017-09-19 12:52:25 +0200

    Install webpack

---
+++ webpack.common.js
@@ -0,0 +1,46 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+
+module.exports = {
+  entry: './client/index.js',
+  plugins: [
+    new CleanWebpackPlugin(['build']),
+    new HtmlWebpackPlugin({
+      template: './client/static/index.html'
+    })
+  ],
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'app.js',
+    publicPath: '/'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.(css)$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '/fonts/[name].[ext]'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  stats: {
+    colors: true
+  }
+}
