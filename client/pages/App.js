9a17272bf608b6428ac510141b9d5e92079f5ebc
nicklasfrahm <nicklas.frahm@gmail.com>
2017-09-20 12:08:17 +0200

    Add hamburger menu

--- client/pages/App.js
+++ client/pages/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Menu } from 'semantic-ui-react'
+import { Menu, Dropdown } from 'semantic-ui-react'
 import { HashRouter, Switch, Route, Redirect, NavLink } from 'react-router-dom'
 import Home from './Home'
 
@@ -8,10 +8,28 @@ const App = props => {
     <HashRouter>
       <div>
         <Menu inverted attached style={{ position: 'relative', zIndex: '100' }}>
+          <Dropdown icon="align justify" className="icon" item>
+            <Dropdown.Menu>
+              <Dropdown.Item as={NavLink} to="/home" activeClassName="active">
+                GPS Test
+              </Dropdown.Item>
+              <Dropdown.Item
+                as={NavLink}
+                to="/positions"
+                activeClassName="active"
+              >
+                Positions
+              </Dropdown.Item>
+              <Dropdown.Item
+                as={NavLink}
+                to="/positions"
+                activeClassName="active"
+              >
+                Paths
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
           <Menu.Item header>INDESY</Menu.Item>
-          <Menu.Item as={NavLink} activeClassName="active" to="/home">
-            Home
-          </Menu.Item>
         </Menu>
         <Switch>
           <Route exact path="/home" component={Home} />
